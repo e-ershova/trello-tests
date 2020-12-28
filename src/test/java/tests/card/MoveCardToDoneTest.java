e7f1e0b38e023d2825f45e1dea4a88436e310de6
annachepkasova <bukovna@yandex.ru>
2020-12-28 17:56:37 +0300

    [qa] finished MoveCardToInProgressTest + added MoveCardToDoneTest and DeleteCardFromDoneTest

--- src/test/java/tests/card/MoveCardToDoneTest.java
+++ src/test/java/tests/card/MoveCardToDoneTest.java
@@ -1,18 +1,16 @@
 package tests.card;
-
 import models.board.TrelloBoard;
 import models.card.TrelloCard;
 import models.list.TrelloList;
 import org.junit.jupiter.api.Test;
 import tests.BaseTest;
-
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class MoveCardToInProgressTest extends BaseTest {
+public class MoveCardToDoneTest extends BaseTest  {
 
     @Test
-    public void moveCardToInProgressList() {
+    public void moveCardToDoneList() {
 
         //создаем доску для списков
         TrelloBoard boardFromPostResponse =
@@ -37,6 +35,8 @@ public class MoveCardToInProgressTest extends BaseTest {
                         .spec(responseSpecification)
                         .extract().as(TrelloList.class);
 
+        String toDoListID = toDo.getId();
+
         //создаем список In Progress
         TrelloList inProgress =
                 given()
@@ -48,8 +48,20 @@ public class MoveCardToInProgressTest extends BaseTest {
                         .spec(responseSpecification)
                         .extract().as(TrelloList.class);
 
-        String toDoListID = toDo.getId();
         String inProgressListID = inProgress.getId();
+
+        //создаем список Done
+        TrelloList done =
+                given()
+                        .spec(listSpec)
+                        .queryParam("name", "Done")
+                        .queryParam("idBoard", boardID)
+                        .post()
+                        .then()
+                        .spec(responseSpecification)
+                        .extract().as(TrelloList.class);
+
+        String doneListID = done.getId();
 
         //создаем карточку в списке To Do
         TrelloCard cardFromPostResponse =
@@ -65,18 +77,16 @@ public class MoveCardToInProgressTest extends BaseTest {
         String cardID = cardFromPostResponse.getId();
 
         //перемещаем карточку в список In Progress
-        TrelloCard updatedCard =
                 given()
                         .spec(cardSpec)
                         .pathParam("id", cardID)
                         .queryParam("idList", inProgressListID)
                         .put("{id}")
                         .then()
-                        .spec(responseSpecification)
-                        .extract().as(TrelloCard.class);
+                        .spec(responseSpecification);
 
         //получаем текущий список карточки
-        TrelloCard getListFromUpdatedCard =
+        TrelloCard getCardList1 =
                 given()
                         .spec(cardSpec)
                         .pathParam("id", cardID)
@@ -86,9 +96,33 @@ public class MoveCardToInProgressTest extends BaseTest {
                         .extract().as(TrelloCard.class);
 
         //проверяем, что текущий лист карточки = In Progress
-        assertThat(getListFromUpdatedCard.getId())
+        assertThat(getCardList1.getId())
                 .as("List of updated card should be In Progress")
                 .isEqualTo(inProgressListID);
 
+        //перемещаем карточку в список Done
+                given()
+                        .spec(cardSpec)
+                        .pathParam("id", cardID)
+                        .queryParam("idList", doneListID)
+                        .put("{id}")
+                        .then()
+                        .spec(responseSpecification);
+
+        //получаем текущий список карточки
+        TrelloCard getCardList2 =
+                given()
+                        .spec(cardSpec)
+                        .pathParam("id", cardID)
+                        .get("{id}/list")
+                        .then()
+                        .spec(responseSpecification)
+                        .extract().as(TrelloCard.class);
+
+        //проверяем, что текущий лист карточки = Done
+        assertThat(getCardList2.getId())
+                .as("List of updated card should be Done")
+                .isEqualTo(doneListID);
     }
+
 }
